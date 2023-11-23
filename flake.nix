dffaf4b5e075f9c7f5754ce9163529c9f37b9041
Thilina Lakshan <thilina.18@cse.mrt.ac.lk>
2023-11-23 07:52:04 +0530

    flake added

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "My NixOS System";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs }: 
+  let 
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { 
+      inherit system;
+      config = { 
+        allowUnfree = true; 
+      };
+    };
+  in
+  {
+    nixosConfigurations = {
+      "TLM-NixOS" = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit system; };
+        modules = [ 
+          "./nixos/configuration.nix" 
+        ];
+      };
+    };
+  };
+}
